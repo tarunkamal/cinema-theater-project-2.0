346288b869f0274285f96f56a3c041abc5650bd2
tarunkamal <tarunkamal5907@gmail.com>
2026-07-05 21:31:47 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,35 @@
 number_of_seats = 150
 correct_password = "secure123"
-a=int(input("Enter the password: "))
-if a!= correct_password:
+
+password = input("Enter the password: ")
+if password != correct_password:
     print("Incorrect password. Access denied.")
-    break
+    raise SystemExit
+
 print("\nPassword accepted. Welcome to the Theater Booking System!")
 
 while True:
-    print("\t---MAIN MENU---")
+    print("\n\t---MAIN MENU---")
     print("1. Book a seat")
     print("2. Check available seats")
     print("3. Exit")
 
-choice = int(input("Enter your choice: "))
+    choice = int(input("Enter your choice: "))
 
-if choice == 1:
-    number = int(input("Enter the number of seats to book: "))
-    if number > number_of_seats:
-        print("Invalid choice! Not enough seats available.")
+    if choice == 1:
+        number = int(input("Enter the number of seats to book: "))
+        if number > number_of_seats:
+            print("Invalid choice! Not enough seats available.")
+        else:
+            number_of_seats -= number
+            print("Congrats! You have successfully booked", number, "seats.")
+
+    elif choice == 2:
+        print("Available seats =", number_of_seats)
+
+    elif choice == 3:
+        print("Thank you for using our service. Goodbye!!")
+        break
+
     else:
-        number_of_seats -= number
-        print("Congrats! You have successfully booked", number, "seats.")
-
-elif choice == 2:
-    print("Available seats =", number_of_seats)
-
-elif choice == 3 :
-    print("Thank you for using our service. Goodbye!!")
-    break
+        print("Invalid option. Please try again.")
